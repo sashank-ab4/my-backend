794e8c39eb9f8f7742073449e7557dd5fbf82381
Sashank <sashankakkabattula@gmail.com>
2026-03-03 12:24:18 +0530

    did basic things with postman -get/post/delete types

--- SRC/app.js
+++ SRC/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
-
 const app = express();
-const port = 3000;
-app.use((req, res) => {
-  res.send("Hi from servers!");
-});
+const port = 4444;
 
-app.use("/dash", (req, res) => {
-  res.send("this is dashboard!");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Sashank", lastName: "AB" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data is saved successfully!");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data is deleted successfully!");
+});
+app.use((req, res) => {
+  res.send("Hi from the server!");
 });
 
 app.listen(port, () => {
-  console.log("Serve connected and listening on port..", port);
+  console.log("Server connected and listening on port..", port);
 });
