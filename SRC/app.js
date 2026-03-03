d2ca254f474a51f946d8d75b4fc0c31ce44666dd
Sashank <sashankakkabattula@gmail.com>
2026-03-03 12:51:52 +0530

    : about routes

--- SRC/app.js
+++ SRC/app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = 4444;
 
-app.get("/user", (req, res) => {
+/* app.get("/user", (req, res) => {
   res.send({ firstName: "Sashank", lastName: "AB" });
+}); */
+
+app.get("/user/:userId/:peru", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Samuel", lastName: "John" });
 });
-app.post("/user", (req, res) => {
+
+/* app.post("/user", (req, res) => {
   res.send("Data is saved successfully!");
 });
 app.delete("/user", (req, res) => {
@@ -13,7 +19,7 @@ app.delete("/user", (req, res) => {
 });
 app.use((req, res) => {
   res.send("Hi from the server!");
-});
+}); */
 
 app.listen(port, () => {
   console.log("Server connected and listening on port..", port);
