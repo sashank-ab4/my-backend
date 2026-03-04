bf3ffd32a8c1e51fdab84cd23f8adc05229e34ac
Sashank <sashankakkabattula@gmail.com>
2026-03-04 21:29:39 +0530

    CRUD operations

--- SRC/app.js
+++ SRC/app.js
@@ -6,7 +6,7 @@ const port = 4444;
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
+  const user = new User(req.body); // post req- creating database
 
   // creating a new instance of user model
   /*const user = new User({
@@ -26,6 +26,56 @@ app.post("/signup", async (req, res) => {
     res.send("User Signed up successfully!");
   } catch (err) {
     res.status(400).send("Error signing up the user:" + err.message);
+  }
+});
+// get USER by EMAIL
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.send(404).send("Something went wrong:");
+  }
+});
+// get all USERS
+app.get("/users", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    res.send(allUsers);
+  } catch (err) {
+    res.send(404).send("Something went wrong!");
+  }
+});
+
+// get user by ID and delete
+
+app.delete("/users", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    /* const user = await User.findByIdAndDelete( userId ); --> shorthand for above code and is acceptible accd to mongoose documentation*/
+    res.send("deleted user!");
+  } catch (err) {
+    res.send(404).send("Something went wrong!");
+  }
+});
+
+// update the existing user
+
+app.patch("/users", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User is updated successfully!");
+    console.log(updatedUser);
+  } catch (err) {
+    res.send(404).send("Something went wrong!");
   }
 });
 
