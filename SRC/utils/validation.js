8b058d236722f8d265b811abf5e19cd89f424795
Sashank <sashankakkabattula@gmail.com>
2026-03-07 12:00:24 +0530

    added new validation for /profile/edit

--- SRC/utils/validation.js
+++ SRC/utils/validation.js
@@ -11,6 +11,47 @@ const validateSignUpData = (req) => {
   }
 };
 
+const validateProfileEditData = (req) => {
+  const data = req.body;
+  const allowedFields = [
+    "firstName",
+    "lastName",
+    "age",
+    "emailId",
+    "about",
+    "skills",
+    "photoUrl",
+  ];
+  const isUpdatesAllowed = Object.keys(data).every((field) =>
+    allowedFields.includes(field),
+  );
+
+  return isUpdatesAllowed;
+  /* if(!isUpdatesAllowed){
+    throw new Error('Editing this field is not allowed!')
+  }
+  
+
+  res.send('Profile is updated Successfully!') */
+};
+/* try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "skills", "gender"];
+    const isUpdatesAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+    if (!isUpdatesAllowed) {
+      throw new Error("Updating this field is not allowed");
+    }
+    if (data?.skills.length > 5) {
+      throw new Error("You cannnot add more than 5 skills!");
+    }
+    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    res.send("User is updated successfully!");
+    console.log(updatedUser); */
+
 module.exports = {
   validateSignUpData,
+  validateProfileEditData,
 };
