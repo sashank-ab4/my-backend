f5d29547fb1aad67c86ea088483daa466503115d
Sashank <sashankakkabattula@gmail.com>
2026-03-10 11:48:54 +0530

    built /feed API with some corner cases like excluding loggedIn user card and few others

--- SRC/routes/users.js
+++ SRC/routes/users.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
+const VISBILE_DATA = "firstName lastName age about skills gender";
 // get all the PENDING connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -36,12 +38,60 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { receiverUserId: loggedInUser._id, status: "accepted" },
         { senderUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("senderUserId", "firstName lastName age about skills gender");
+    })
+      .populate("senderUserId", VISBILE_DATA)
+      .populate("receiverUserId", VISBILE_DATA);
 
-    const data = connectionRequests.map((row) => row.senderUserId);
-    res.send("showing connections").json({ data });
+    const data = connectionRequests.map((row) => {
+      if (row.senderUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.receiverUserId;
+      }
+      return row.senderUserId;
+    });
+    res.json({ data });
   } catch (err) {
     res.status(400).send("ERROR:" + err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    // feed is where logged in user get all the users he wants to show interest,ignore,accept,reject
+    // logged in user should see all the user cards except
+    // 1. his own card
+    // 2. already "interested" cards
+    // 3. loggedIn user's ignored cards
+    // 4. loggedIn user's accepted connections
+    const loggedInUser = req.user;
+    // find all the connection requests (sent and received)
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [
+        { senderUserId: loggedInUser._id },
+        { receiverUserId: loggedInUser._id },
+      ],
+    }).select("senderUserId receiverUserId");
+
+    const usersShouldNotBeInFeed = new Set();
+    connectionRequests.forEach((req) => {
+      usersShouldNotBeInFeed.add(req.senderUserId.toString());
+      usersShouldNotBeInFeed.add(req.receiverUserId.toString());
+    });
+    // DB Query 1. users not in the restricted array, 2. same loggedIn user
+    const usersShouldBeInFeed = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(usersShouldNotBeInFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(VISBILE_DATA);
+
+    res.json({
+      message: "Core Feed",
+      success: true,
+      count: usersShouldBeInFeed.length,
+      data: usersShouldBeInFeed,
+    });
+  } catch (err) {
+    res.status(401).send("Unable to load the feed " + err.message);
   }
 });
 module.exports = userRouter;
