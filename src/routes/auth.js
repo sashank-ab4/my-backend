4e1f9dad6e2f17bccfc2d5570611e87a08d5be0e
Sashank <sashankakkabattula@gmail.com>
2026-03-24 15:16:40 +0530

    fixed a small bug

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -108,7 +108,7 @@ authRouter.post("/reset-password/:token", async (req, res) => {
   const { password } = req.body;
   const { token } = req.params;
 
-  const decoded = jwt.verify(token, "YekTerSec$44");
+  const decoded = jwt.verify(token, JWT_KEY);
   const user = await User.findById(decoded._id);
   const hashedPassword = await bcrypt.hash(password, 10);
   user.password = hashedPassword;
